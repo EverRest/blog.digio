d73dd928502e730bedd0b421575f69f106b10950
EverRest <medynskyypavlo@gmail.com>
2024-09-07 13:23:52 +0200

    added permissions

--- init.sh
+++ init.sh
@@ -4,6 +4,10 @@ mkdir -p ./mariadb-persistence
 mkdir -p ./wordpress-persistence
 mkdir -p ./nginx/conf/server_blocks
 
+chown -R 1001:1001 ./mariadb-persistence
+chown -R 1001:1001 ./wordpress-persistence
+chown -R 1001:1001 ./nginx
+
 if [ -f ./default.conf ]; then
   mv ./default.conf ./nginx/conf/server_blocks/default.conf
 else
